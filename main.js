b5a47ba02e1ef67848e27ef6fc0031ca360d836d
zoink506 <benhubble03@gmail.com>
2022-12-01 13:25:00 +1100

    Add input for numbers and partially for operators

--- main.js
+++ main.js
@@ -14,51 +14,74 @@ function divide(value1, value2) {
   return parseFloat(value1) / parseFloat(value2);
 }
 
-let equation = [];
-function calculator() {
-  let answer;
-  let value1;
-
-  if(equation.length === 0) {
-    value1 = prompt("Enter first number");
-  } else {
-    value1 = equation[0];
-  }
-
-  const operator = prompt("Enter operator");
-  const value2 = prompt("Enter second number");
-  equation[0] = value1;
-  equation[1] = operator;
-  equation[2] = value2;
-
-  if(equation[1] === "+") {
-    answer = add(equation[0], equation[2]);
-  } else if(equation[1] === "-") {
-    answer = subtract(equation[0], equation[2]);
-  } else if(equation[1] === "*") {
-    answer = multiply(equation[0], equation[2])
-  } else if(equation[1] === "/") {
-    answer = divide(equation[0], equation[2]);
-  } else {
-    answer = "ERROR";
-  }
-
-  console.log(equation);
-  equation[0] = answer;
-  equation.splice(1, 2);
-
+function makeEventListeners() {
+  const buttons = document.querySelectorAll(".button");
   
-  console.log("test");
-  console.log(equation);
-  calculator();
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      operate(button);
+    });
+  });
 }
 
+let equation = [];
+console.log(equation);
+function operate(input) {
 
-//console.log(add(3, 5));      // 8
-//console.log(subtract(5, 2)); // 3
-//console.log(multiply(3, 4)); // 12
-//console.log(divide(8, 2));   // 4
+  let inputType;
+  if(input.classList.contains("number")) {
+    inputType = "number";
+  } else if(input.classList.contains("operator")) {
+    inputType = "operator";
+  } else if(input.id === "=") {
+    inputType = "equals";
+  }
+
+  /*
+   * If the second value is defined, add the input to the second value
+   * 
+   * If the second value is undefined, check if the operator is defined
+   *   - If the operator is defined, then add the input to the second value
+   *   - If the operator is not defined, then add the input to first value
+   *      - If the first value is defined, add input to the first value
+   *      - If the first value is undefined, set first value to be the input
+   */
+
+  if(inputType === "number") {
+    if(equation[2] === undefined) {
+      if(equation[1] === undefined) {
+        if(equation[0] === undefined) {
+          equation[0] = input.innerText;
+        } else {
+          equation[0] += input.innerText;
+        }
+      } else {
+        equation[2] = input.innerText;
+      }
+    } else {
+      equation[2] += input.innerText;
+    }
+
+  } else if(inputType === "operator") {
+    if(equation[0] !== undefined) {
+      if(equation[2] === undefined) {
+        // only first value has been input
+        // set equation[1] to the operator
+        equation[1] = input.innerText;
+      } else {
+        // first and second values have been input
+        // evaluate equation, then set answer to first value, and the selected operator to equation[1]
+        // do later
+      }
+    }
+
+  } else if(inputType === "equals") {
 
 
-calculator();
-//makeEventListeners();
+  }
+
+  console.log(equation);
+}
+
+//calculator();
+makeEventListeners();
